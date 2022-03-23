27c58bc46dc24f8ca0f508861b1fa13ef3d90d58
Poloeli303 <91678247+Poloeli303@users.noreply.github.com>
2022-03-23 09:40:44 -0600

    Update sprite animation.py

--- sprite animation.py
+++ sprite animation.py
@@ -6,20 +6,21 @@ screen.fill((0,0,0))
 clock = pygame.time.Clock() #set up clock
 gameover = False #variable to run our game loop
 
-Link = pygame.image.load('link.png') #load your spritesheet
+Link = pygame.image.load('ash1.png') #load your spritesheet
 Link.set_colorkey((255, 0, 255)) #this makes bright pink (255, 0, 255) transparent (sort of)
 
 #player variables
 xpos = 500 #xpos of player
-ypos = 200 #ypos of player
+ypos = 500-18 #ypos of player
 vx = 0 #x velocity of player
-vy = 0
 keys = [False, False, False, False] #this list holds whether each key has been pressed
 
+LEFT=0
+RIGHT = 1
 
 #animation variables variables
-frameWidth = 64
-frameHeight = 96
+frameWidth = 47
+frameHeight = 52
 RowNum = 0 #for left animation, this will need to change for other animations
 frameNum = 0
 ticker = 0
@@ -36,20 +37,12 @@ while not gameover:
                 keys[0]=True
             elif event.key == pygame.K_RIGHT:
                 keys[1]=True
-            elif event.key == pygame.K_UP:
-                keys[2]=True
-            elif event.key == pygame.K_DOWN:
-                keys[3]=True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 keys[0]=False
             elif event.key == pygame.K_RIGHT:
                 keys[1]=False
-            elif event.key == pygame.K_UP:
-                keys[2]=False
-            elif event.key == pygame.K_DOWN:
-                keys[3]=False
-            
+          
 
     #LEFT MOVEMENT
     if keys[0]==True:
@@ -58,86 +51,46 @@ while not gameover:
     #RIGHT MOVEMENT
     elif keys[1] == True:
         vx = 3
-        direction = 0
-    elif keys[2] == True:
-        vy = -3
-        direction = 0
-    elif keys[3] == True:
-        vy = 3
-        direction = 0
     #turn off velocity
     else:
         vx = 0
-        vy = 0
         
     #UPDATE POSITION BASED ON VELOCITY
         
     xpos+=vx #update player xpos
-    ypos+=vy
+        
     #ANIMATION-------------------------------------------------------------------
         
     # Update Animation Information
     # Only animate when in motion
-    if vx < 0:#left animation
-        # Ticker is a spedometer. We don't want Link animating as fast as the
-        # processor can process! Update Animation Frame each time ticker goes over
+    if vx>0:
+        RowNum = 0
+    if vx < 0: #left animation
+        RowNum = 2
         ticker+=1
-        if ticker%10==0: #only change frames every 10 ticks
-            frameNum+=1
-           #If we are over the number of frames in our sprite, reset to 0.
-           #In this particular case, there are 10 frames (0 through 9)
-        if frameNum>7: 
-            frameNum = 0
-    elif vx > 0:#left animation
-        # Ticker is a spedometer. We don't want Link animating as fast as the
-        # processor can process! Update Animation Frame each time ticker goes over
+        if ticker%10==0:
+          frameNum+=1
+           
+        if frameNum>3: 
+           frameNum = 0
+           
+    if vx > 0: #left animation
+        RowNum = 3
         ticker+=1
-        if ticker%10==0: #only change frames every 10 ticks
-            frameNum+=1
-           #If we are over the number of frames in our sprite, reset to 0.
-           #In this particular case, there are 10 frames (0 through 9)
-        if frameNum>7: 
-            frameNum = 0
-    elif vy < 0:#left animation
-        # Ticker is a spedometer. We don't want Link animating as fast as the
-        # processor can process! Update Animation Frame each time ticker goes over
-        ticker+=1
-        if ticker%10==0: #only change frames every 10 ticks
-            frameNum+=1
-           #If we are over the number of frames in our sprite, reset to 0.
-           #In this particular case, there are 10 frames (0 through 9)
-        if frameNum>7: 
-            frameNum = 0
-    elif vy > 0:#left animation
-        # Ticker is a spedometer. We don't want Link animating as fast as the
-        # processor can process! Update Animation Frame each time ticker goes over
-        ticker+=1
-        if ticker%10==0: #only change frames every 10 ticks
-            frameNum+=1
-           #If we are over the number of frames in our sprite, reset to 0.
-           #In this particular case, there are 10 frames (0 through 9)
-        if frameNum>7: 
-            frameNum = 0
-  
+        if ticker%10==0:
+          frameNum+=1
+           
+        if frameNum>3: 
+           frameNum = 0
     # RENDER--------------------------------------------------------------------------------
     # Once we've figured out what frame we're on and where we are, time to render.
             
-    screen.fill((0,0,0)) #wipe screen so it doesn't smear
-    if keys[0] == True:
-        RowNum=0
-        screen.blit(Link, (xpos, ypos), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))
-    elif keys[1] == True:
-        RowNum=1
-        screen.blit(Link, (xpos, ypos), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))
-    elif keys[2] == True:
-        RowNum=2
-        screen.blit(Link, (xpos, ypos), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))
-    elif keys[3] == True:
-        RowNum=3
-        screen.blit(Link, (xpos, ypos), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight)) 
-    else:
-        screen.blit(Link, (xpos, ypos), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))
+    screen.fill((255,255,255)) #wipe screen so it doesn't smear
+    screen.blit(Link, (xpos, ypos), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))
+    pygame.draw.rect(screen, (0, 130, 50), (0, 535, 800, 500))
     pygame.display.flip()#this actually puts the pixel on the screen
     
 #end game loop------------------------------------------------------------------------------
 pygame.quit()
+
+
